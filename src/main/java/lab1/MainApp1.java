e920e29c7cb75b4d523583cc591ea519ea4326a7
Richard Blažek <blazek.risa@gmail.com>
2024-09-10 14:00:59 +0800

    Clearer output message

--- src/main/java/lab1/MainApp1.java
+++ src/main/java/lab1/MainApp1.java
@@ -2,10 +2,8 @@ package lab1;
 
 public class MainApp1 {
     public static void main(String[] args) {
-        int b = 2;
-        int n = 11;
-        System.out.println(b + " to power " + n + " = " + MyLibrary.power(b, n));
-        System.out.println(n + "! = " + MyLibrary.factorial(n));
-        System.out.println("Program Ended ...");
+        System.out.printf("2 to the 11th power = %d\n", MyLibrary.power(2, 11));
+        System.out.printf("The factorial of 11 = %d\n", MyLibrary.factorial(11));
+        System.out.println("Program ended");
     }
 }
