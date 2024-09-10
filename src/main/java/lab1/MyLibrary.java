daf625cfccefbe48a339ea68f868b22dd03f808f
Richard Blažek <blazek.risa@gmail.com>
2024-09-10 14:01:09 +0800

    Optimized computation

--- src/main/java/lab1/MyLibrary.java
+++ src/main/java/lab1/MyLibrary.java
@@ -1,8 +1,14 @@
 package lab1;
 
 public class MyLibrary {
+    private static int power(int base, int exponent, int acc) {
+        if (exponent == 0) return acc;
+        if (exponent % 2 == 0) return power(base * base, exponent / 2, acc);
+        return power(base, exponent - 1, acc * base);
+    }
+
     public static int power(int base, int exponent) {
-        return exponent == 0 ? 1 : base * power(base, exponent - 1);
+        return power(base, exponent, 1);
     }
 
     public static int factorial(int n) {
